e8f7fc457583a5fc8446b9e9a710e3e10d5ec262
Swan <swanand556@gmail.com>
2025-12-08 17:13:13 +0530

    logic complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,75 @@
 <script lang="ts">
+	const max_x: number = 1407;
+	const max_y: number = 712;
+
+	const origin_true: { x: number; y: number } = { x: max_x / 2, y: max_y / 2 };
+	const eye_left_origin: { x: number; y: number } = { x: 60, y: 0 };
+	const eye_right_origin: { x: number; y: number } = { x: -60, y: 0 };
 	let pointer_coor: { x: number; y: number } = $state({ x: 0, y: 0 });
+	let dir_from_left_eye: { x: number; y: number } = $derived({
+		x: pointer_coor.x - eye_left_origin.x,
+		y: pointer_coor.y - eye_left_origin.y
+	});
+	let dir_from_right_eye: { x: number; y: number } = $derived({
+		x: pointer_coor.x - eye_right_origin.x,
+		y: pointer_coor.y - eye_right_origin.y
+	});
+
+	let dist_from_left_eye: number = $derived(
+		Math.sqrt(Math.pow(dir_from_left_eye.x, 2) + Math.pow(dir_from_left_eye.y, 2))
+	);
+	let dist_from_right_eye: number = $derived(
+		Math.sqrt(Math.pow(dir_from_right_eye.x, 2) + Math.pow(dir_from_right_eye.y, 2))
+	);
+
+	let left_eye_dir_theta: number = $derived(Math.atan(dir_from_left_eye.y / dir_from_left_eye.x));
+	let right_eye_dir_theta: number = $derived(
+		Math.atan(dir_from_right_eye.y / dir_from_right_eye.x)
+	);
+
+	let translate_dist_left: { dx_t: number; dy_t: number } = $derived({
+		dx_t:
+			Math.min(dist_from_left_eye, 30) *
+			Math.cos(left_eye_dir_theta) *
+			(dir_from_left_eye.x > 0 ? -1 : 1),
+		dy_t:
+			Math.min(dist_from_left_eye, 30) *
+			Math.sin(left_eye_dir_theta) *
+			(dir_from_left_eye.x > 0 ? -1 : 1)
+	});
+	let translate_dist_right: { dx_t: number; dy_t: number } = $derived({
+		dx_t:
+			Math.min(dist_from_right_eye, 30) *
+			Math.cos(right_eye_dir_theta) *
+			(dir_from_right_eye.x > 0 ? -1 : 1),
+		dy_t:
+			Math.min(dist_from_right_eye, 30) *
+			Math.sin(right_eye_dir_theta) *
+			(dir_from_right_eye.x > 0 ? -1 : 1)
+	});
+
 	const onpointermove = (event: any) => {
-		pointer_coor.x = event.clientX;
-		pointer_coor.y = event.clientY;
+		pointer_coor.x = -event.clientX + origin_true.x;
+		pointer_coor.y = -event.clientY + origin_true.y;
 	};
 </script>
 
 <div class="container" {onpointermove}>
 	<div class="eyes">
 		<div class="eye">
-			<div class="pupil left">
-				<p>{Math.round(pointer_coor.x)}</p>
+			<div
+				class="pupil"
+				style={`transform: translate(${translate_dist_left.dx_t}px, ${translate_dist_left.dy_t}px);`}
+			>
+				<!-- <p>{Math.round(pointer_coor.x)}</p> -->
 			</div>
 		</div>
 		<div class="eye">
-			<div class="pupil right">
-				<p>{Math.round(pointer_coor.y)}</p>
+			<div
+				class="pupil"
+				style={`transform: translate(${translate_dist_right.dx_t}px, ${translate_dist_right.dy_t}px);`}
+			>
+				<!-- <p>{Math.round(pointer_coor.y)}</p> -->
 			</div>
 		</div>
 	</div>
@@ -45,7 +99,7 @@
 		justify-content: center;
 	}
 	.eye {
-		background-color: azure;
+		background-color: rgb(238, 16, 16);
 		width: 100px;
 		height: 100px;
 		border: 0px solid;
@@ -57,10 +111,11 @@
 	}
 
 	.pupil {
-		background-color: rgb(0, 169, 169);
-		width: 20px;
-		height: 40px;
-		border: 0px solid;
+		background-color: rgb(0, 0, 0);
+		width: 30px;
+		height: 30px;
+		border: 3px solid rgb(7, 103, 147);
 		border-radius: 50%;
+		box-sizing: border-box;
 	}
 </style>
